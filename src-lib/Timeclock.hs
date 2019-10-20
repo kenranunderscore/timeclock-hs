ed327b47af3ab747f62a585bc353528d5421ef2a
kenranunderscore <johb.maier@gmail.com>
2019-10-20 15:07:24 +0200

    Add custom miniature type for date+time

--- src-lib/Timeclock.hs
+++ src-lib/Timeclock.hs
@@ -1,1 +1,27 @@
 module Timeclock where
+
+import qualified Data.Time.Calendar as Calendar
+import           Time               (Time, mkTime)
+
+data DateTime =
+  DateTime
+  { date :: Calendar.Day
+  , time :: Time
+  }
+
+mkDateTime
+  :: Integer
+  -> Int
+  -> Int
+  -> Int
+  -> Int
+  -> Int
+  -> Maybe DateTime
+mkDateTime y m d h mi s = do
+  time <- mkTime h mi s
+  day <- Calendar.fromGregorianValid y m d
+  return $ DateTime day time
+
+data Entry
+  = ClockIn DateTime String
+  | ClockOut DateTime (Maybe String)
